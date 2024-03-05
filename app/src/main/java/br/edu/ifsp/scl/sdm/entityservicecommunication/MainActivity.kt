f25fbee2c98c64d359051abb01a1aae923bb720e
northonh <nobile.pedro@gmail.com>
2024-03-05 13:32:36 -0300

    Commit inicial com projeto base vazio.

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -0,0 +1,28 @@
+package br.edu.ifsp.scl.sdm.entityservicecommunication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.sdm.entityservicecommunication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+        with (amb) {
+            mainTb.apply {
+                getString(R.string.app_name).also { setTitle(it) }
+                setSupportActionBar(this)
+            }
+            incrementBt.setOnClickListener {
+
+            }
+        }
+    }
+}
